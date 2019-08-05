09cdf645272893e5ced0671420f48ec6f5efc8b0
Jamie Vullo <jvullo@protonmail.com>
2019-08-05 15:42:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require_relative 'config/environment'
 
+
+	get '/' do
+      erb :index
+  end
+  
+  
 class App < Sinatra::Base
 
 	get '/' do
 		"<h1>Hello World</h1>"
 	end
 	
-	get '/' do
-      erb :index
-  end
+
   
 end
